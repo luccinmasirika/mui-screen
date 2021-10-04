a7c215cfac6ee50ef1181d19cb6f3e4f7ca9273c
Luccin Masirika <luccinmasirika@gmail.com>
2021-10-04 14:01:25 +0100

    layout refactoring

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,8 @@
 import { styled } from '@mui/system';
-import { grey } from '@mui/material/colors';
 
 const Root = styled('main')(({ theme }) => ({
   padding: `${theme.spacing(3)} ${theme.spacing(1)}`,
-  background: grey[200],
+  background: theme.palette.grey[200],
   minHeight: '100vh',
   display: 'flex',
   justifyContent: 'center',
