4417ab97e8bdb822441a3908f793cbe9e368bc92
Luccin Masirika <luccinmasirika@gmail.com>
2021-10-06 17:52:22 +0100

    header

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { AppBar } from '../../components';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/system';
+import { Container } from '@mui/material';
+import Button from '@mui/material/Button';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const Header = styled('div')(({ theme }) => ({
+  margin: `${theme.spacing(5)} 0`,
+  display: 'flex',
+  justifyContent: 'space-between',
+}));
 
 const Dashboard = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar />
+      <Container>
+        <Header>
+          <Button variant='contained'>Add Customer</Button>
+          <Button startIcon={<SettingsIcon />}>Settings</Button>
+        </Header>
+      </Container>
     </Box>
   );
 };
